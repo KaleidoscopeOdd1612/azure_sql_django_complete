413b2246cb4d0f5367d1686307ef90c9148a3cc3
kaidoscope <sirawit.hrg@gmail.com>
2026-02-05 15:13:13 +0700

    Please enter the commi# with '#' will be ignored, and an empty message aborts the commit. Modified setting.py

--- azure_project/settings.py
+++ azure_project/settings.py
@@ -90,10 +90,10 @@ WSGI_APPLICATION = 'azure_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'mssql',
-        'NAME': 'bearlab-sql-db',
-        'USER': 'bearlab-admin',
-        'PASSWORD': 'P@ss1234',
-        'HOST': 'bearlab-sql-server.database.windows.net',
+        'NAME': 'free-sql-db-8144856',
+        'USER': 'Myass',
+        'PASSWORD': 'Ass88888',
+        'HOST': 'gun-sql-server.database.windows.net',
         'PORT': '1433',
         'OPTIONS': {
             'driver': 'ODBC Driver 18 for SQL Server',
